21099392164f6ccc0c3742434266134eaddcdff5
Robert Jackson <robert.w.jackson@me.com>
2015-02-09 19:06:00 -0500

    Add some nested components in various locations.

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'components-in-subdirs',
+    podModulePrefix: 'components-in-subdirs/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
